630e5cb62f199f93b5e7778b92030e816d266659
webzongmin <418040558@qq.com>
2016-10-21 11:25:15 +0800

    aa

--- ApplePlan/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(".tab-list li").on("click",function(){
-		$(this).addClass("add").siblings().removeClass("add");
-		var ind=$(this).index();
-		$(".list").hide();
-		$(".list").eq(ind).show();
-	})
-})
